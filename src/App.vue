e10141a629e42f9c39e162ca146374ad6fdf8ee8
Venky-2107 <chvenkatpatiraju@gmail.com>
2023-02-14 20:03:33 +0530

    additional changes
    additional changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/pari-logo.webp" />
   <HelloWorld msg="Welcome to Your *****PARI App*****" />
 </template>
 
